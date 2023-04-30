16683a9bef86e611c2c686df6071c444b1c086db
Nha1410 <thanhnha141001@gmail.com>
2023-04-30 15:32:23 +0700

    processing

--- bktel/resources/js/components/PlantDiseasesComponent.vue
+++ bktel/resources/js/components/PlantDiseasesComponent.vue
@@ -1,20 +1,20 @@
 <template>
-    <div class="relative min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8 bg-gray-500 bg-no-repeat bg-cover relative items-center"
+    <div class="relative h-[92vh] flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8 bg-gray-500 bg-no-repeat bg-cover relative items-center"
         style="background-image: url(https://images.unsplash.com/photo-1621243804936-775306a8f2e3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80);">
-        <div class="absolute bg-black opacity-60 inset-0 z-0"></div>
+        <div class="absolute bg-black opacity-40 inset-0 z-0"></div>
         <div class="sm:max-w-lg w-full p-10 bg-white rounded-xl z-10">
             <div class="text-center">
-                <h2 class="mt-5 text-3xl font-bold text-gray-900">
+                <h2 class="text-3xl font-bold text-gray-900">
                     File Upload!
                 </h2>
-                <p class="mt-2 text-sm text-gray-400">Lorem ipsum is placeholder text.</p>
+                <p class="mt-2 text-sm text-gray-400">Put your leaf into here</p>
             </div>
             <div class="mt-8 space-y-3">
                 <div class="grid grid-cols-1 space-y-2">
                     <label class="text-sm font-bold text-gray-500 tracking-wide">Title</label>
                     <input
                         class="text-base p-2 border border-gray-300 rounded-lg focus:outline-none focus:border-indigo-500"
-                        type="" placeholder="mail@gmail.com">
+                        type="" placeholder="">
                 </div>
                 <div class="grid grid-cols-1 space-y-2">
                     <label class="text-sm font-bold text-gray-500 tracking-wide">Attach Document</label>
@@ -41,18 +41,24 @@
                     <span>File type: doc,pdf,types of images</span>
                 </p>
                 <div>
-                    <button  @click="uploadImage"
-                        class="my-5 w-full flex justify-center bg-blue-500 text-gray-100 p-4  rounded-full tracking-wide
+                    <button @click="uploadImage"
+                        class="w-full flex justify-center bg-blue-500 text-gray-100 p-4  rounded-full tracking-wide
                                 font-semibold  focus:outline-none focus:shadow-outline hover:bg-blue-600 shadow-lg cursor-pointer transition ease-in duration-300">
                         Upload
                     </button>
                 </div>
             </div>
         </div>
+        <div class="vl-parent">
+            <Loading :isActive="isActive" :active="isActive"  :can-cancel="true" :on-cancel="onCancel" :is-full-page="fullPage" style="z-index: 1000;"/>
+            <label><input type="checkbox" v-model="fullPage">Full page?</label>
+        </div>
     </div>
 </template>
 
 <script >
+import Loading from 'vue-loading-overlay';
+import 'vue-loading-overlay/dist/vue-loading.css';
 export default {
     props: {
         predictDiseaseUrl: {
@@ -63,9 +69,17 @@ export default {
         return {
             imageFile: null,
             imageUrl: null,
+            isActive: true,
+            fullPage: true
         }
     },
+    components: {
+        Loading
+    },
     methods: {
+        onCancel() {
+            this.isActive = false;
+        },
         selectFile() {
             this.$refs.fileInput.click();
         },
